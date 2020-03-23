f0c7483114ec40155cdeed98df4ea2198761b97c
sujitbidawe <bidawesujit@gmail.com>
2020-03-23 14:57:06 +0530

    added: code to roll dice

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,7 +5,16 @@ echo "~~~~~Let's play Snake and Ladder~~~~~"
 currentPosition=0
 
 function showCurrentPosition(){
+
 echo "current position is: $currentPosition"
+
+}
+
+function rollDice(){
+
+dice=$((RANDOM%6+1))
+
 }
 
 showCurrentPosition
+rollDice
