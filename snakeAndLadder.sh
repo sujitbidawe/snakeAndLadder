0911745ee90b8be450e45251865213fe2a6c7463
sujitbidawe <bidawesujit@gmail.com>
2020-03-23 16:00:23 +0530

    added: code to repeat playing until player reaches winning position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,6 +5,7 @@ echo "~~~~~Let's play Snake and Ladder~~~~~"
 startingPosition=0
 currentPosition=0
 winningPosition=100
+rollCount=0
 
 function showCurrentPosition(){
 
@@ -18,37 +19,45 @@ dice=$((RANDOM%6+1))
 moveOption=$(( RANDOM%3+1 ))
 
 }
-showCurrentPosition
-rollDice
+#showCurrentPosition
+#rollDice
 
 
 noPlay=1
 ladder=2
 snake=3
 
-case $moveOption in
-		$noPlay)
-				echo "player stays at same position $currentPosition";;
+while [[ $currentPosition -ne $winningPosition ]]
+do
+		showCurrentPosition
+		rollDice
+		(( rollCount++ ))
+		case $moveOption in
+				$noPlay)
+						echo "player stays at same position $currentPosition";;
 
-		$ladder)
-				if [[ $(( $currentPosition + $dice )) -le $winningPosition ]]
-				then
-						currentPosition=$(( $currentPosition + $dice ))
-						echo "player moves forward by $dice positions and is now at $currentPosition"
-				else
-						echo "player stays at same position since $currentPosition+$dice exceeds winning position."
-				fi
-				;;
+				$ladder)
+						if [[ $(( $currentPosition + $dice )) -le $winningPosition ]]
+						then
+								currentPosition=$(( $currentPosition + $dice ))
+								echo "player moves forward by $dice positions and is now at $currentPosition"
+						else
+								echo "player stays at same position since $currentPosition+$dice exceeds winning position."
+						fi
+						;;
 
-		$snake)
-				if [[ $(($currentPosition - $dice)) -lt $startingPosition ]]
-				then
-						currentPosition=0
-						echo "player has came back to the starting position $currentPosition"
-				else
-						currentPosition=$(( $currentPosition - $dice ))
-						echo "player moves backward by $dice positions and is now at $currentPosition"
-				fi
-				;;
-esac
+				$snake)
+						if [[ $(($currentPosition - $dice)) -lt $startingPosition ]]
+						then
+								currentPosition=0
+								echo "player has came back to the starting position $currentPosition"
+						else
+								currentPosition=$(( $currentPosition - $dice ))
+								echo "player moves backward by $dice positions and is now at $currentPosition"
+						fi
+						;;
+		esac
+done
 
+echo "player won!!"
+echo "turns required: $rollCount"
