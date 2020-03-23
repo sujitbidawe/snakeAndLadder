5b6597d5c4aa5b03d2fd7ae80dfb2c9c15923aa6
sujitbidawe <bidawesujit@gmail.com>
2020-03-23 23:50:50 +0530

    added and refactored: code to play with two players

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,17 +1,26 @@
 #! /usr/local/bin/bash
 
-echo "~~~~~Let's play Snake and Ladder~~~~~"
+echo "~~~~~Let's play SNAKE and LADDER~~~~~"
 
-startingPosition=0
-currentPosition=0
-winningPosition=100
+#Constants
+STARTING_POSITION=0
+WINNING_POSITION=100
+NO_PLAY=1
+LADDER=2
+SNAKE=3
+
+#variables
 rollCount=0
+player=1
+currentPositionPlayer1=$STARTING_POSITION
+currentPositionPlayer2=$STARTING_POSITION
 
 function showCurrentPosition(){
 
-echo "current position is: $currentPosition"
+echo "current positions--> player1:$currentPositionPlayer1 player2:$currentPositionPlayer2"
 
 }
+
 
 function rollDice(){
 
@@ -19,45 +28,61 @@ dice=$((RANDOM%6+1))
 moveOption=$(( RANDOM%3+1 ))
 
 }
-#showCurrentPosition
-#rollDice
 
 
-noPlay=1
-ladder=2
-snake=3
-
-while [[ $currentPosition -ne $winningPosition ]]
+while [[ $currentPositionPlayer1 -ne $WINNING_POSITION && $currentPositionPlayer2 -ne $WINNING_POSITION ]]
 do
 		showCurrentPosition
 		rollDice
 		(( rollCount++ ))
-		case $moveOption in
-				$noPlay)
-						echo "player stays at same position $currentPosition";;
 
-				$ladder)
-						if [[ $(( $currentPosition + $dice )) -le $winningPosition ]]
+		if [[ $player -eq 1 ]]
+		then
+				tempPositionHolder=$currentPositionPlayer1
+		else
+				tempPositionHolder=$currentPositionPlayer2
+		fi
+		case $moveOption in
+				$NO_PLAY)
+						echo "player$player stays at same position $tempPositionHolder";;
+
+				$LADDER)
+						if [[ $(( $"$tempPositionHolder" + $dice )) -le $WINNING_POSITION ]]
 						then
-								currentPosition=$(( $currentPosition + $dice ))
-								echo "player moves forward by $dice positions and is now at $currentPosition"
+								tempPositionHolder=$(( $"$tempPositionHolder"+$dice ))
+								echo "player$player moves forward by $dice positions"
 						else
-								echo "player stays at same position since $currentPosition+$dice exceeds winning position."
+								echo "player$player stays at same position since $tempPositionHolder+$dice exceeds winning position."
 						fi
 						;;
 
-				$snake)
-						if [[ $(($currentPosition - $dice)) -lt $startingPosition ]]
+				$SNAKE)
+						if [[ $(( $"$tempPositionHolder" - $dice )) -lt $STARTING_POSITION ]]
+
 						then
-								currentPosition=0
-								echo "player has came back to the starting position $currentPosition"
+								tempPositionHolder=0
+								echo "player$player has came back to the starting position"
 						else
-								currentPosition=$(( $currentPosition - $dice ))
-								echo "player moves backward by $dice positions and is now at $currentPosition"
+								tempPositionHolder=$(( $"$tempPositionHolder" - $dice ))
+								echo "player$player moves backward by $dice positions"
 						fi
 						;;
 		esac
+
+		if [[ $player -eq 1 ]]
+		then
+				currentPositionPlayer1=$tempPositionHolder
+		else
+				currentPositionPlayer2=$tempPositionHolder
+		fi
+		player=$(( 3 - $player  ))
+
 done
 
-echo "player won!!"
+if [[ $currentPositionPlayer1 -eq $WINNING_POSITION ]]
+then
+		echo "player1 won!"
+else
+		echo "player2 won!"
+fi
 echo "turns required: $rollCount"
