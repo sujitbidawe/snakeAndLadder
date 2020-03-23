37d311a623523ff66aa5dce5249e8c97410b7b46
sujitbidawe <bidawesujit@gmail.com>
2020-03-23 14:54:33 +0530

    added: code to start start game with single player at position 0

---
+++ snakeAndLadder.sh
@@ -0,0 +1,11 @@
+#! /usr/local/bin/bash
+
+echo "~~~~~Let's play Snake and Ladder~~~~~"
+
+currentPosition=0
+
+function showCurrentPosition(){
+echo "current position is: $currentPosition"
+}
+
+showCurrentPosition
